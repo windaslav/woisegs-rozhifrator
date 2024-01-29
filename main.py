b4ecc60df9078f850fdedc61d10d59c7dd438288
windaslav <157972135+windaslav@users.noreply.github.com>
2024-01-29 20:16:23 +0700

    теперь, вы можете ввисти команду /status, и бот покажет вам на сколько процентов загружен диск, процессор, и оперативная память на сервере, где находится этот бот, это может быть полезно тем, кто использует этот код на своём хосте, и хочет получать эту информацию прямо из telegram.

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import telebot
 import speech_recognition as sr
 from pydub import AudioSegment
 import requests
+import psutil
 
-bot = telebot.TeleBot("вставьте суда свой токен")
+bot = telebot.TeleBot("вставьте суда токен от бота")
 
 # Функция для обработки голосовых сообщений
 @bot.message_handler(content_types=['voice'])
 def voice_to_text(message):
     file_info = bot.get_file(message.voice.file_id)
-    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format("в ведите суда свой токен от бота", file_info.file_path))
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format("вставьте суда свой токен от бота", file_info.file_path))
     with open('voice.ogg', 'wb') as f:
         f.write(file.content)
 
@@ -24,8 +25,21 @@ def voice_to_text(message):
         text = recognizer.recognize_google(audio_data, language="ru-RU")
         bot.reply_to(message, text)
     except sr.UnknownValueError:
-        bot.reply_to(message, "Извините, не удалось распознать голосовое сообщение, идите нахер, или попробуйте ещё раз")
+        bot.reply_to(message, "Извините, не удалось распознать голосовое сообщение")
     except sr.RequestError:
         bot.reply_to(message, "Извините, возникла проблема с обработкой вашего запроса")
+
+# Функция для обработки текстовых сообщений
+@bot.message_handler(commands=['status'])
+def send_status(message):
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+    memory_percent = memory.percent
+    disk = psutil.disk_usage('/')
+    disk_percent = disk.percent
+    response = f"Загрузка CPU: {cpu_percent}%\n" \
+               f"Загрузка памяти: {memory_percent}%\n" \
+               f"Загрузка диска: {disk_percent}%"
+    bot.reply_to(message, response)
 
 bot.polling()
